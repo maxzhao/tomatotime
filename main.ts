05fad3198f2d0b1196786b0b582fa8e1679b5736
max <maxzhao@users.noreply.github.com>
2021-09-10 00:45:07 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+input.onGesture(Gesture.ScreenDown, function () {
+    if (全局阶段str.compare("工作倒计时") == 0) {
+        if (局部阶段num == 0) {
+            局部阶段num = 1
+        }
+    }
+})
 function 等待阶段结束 (阶段Str: string) {
     while (全局阶段str.compare(阶段Str) == 0) {
         basic.pause(100)
@@ -62,27 +69,53 @@ input.onButtonPressed(Button.A, function () {
                 长休息长度m = 15
             }
         }
-    } else {
-    	
+    } else if (全局阶段str.compare("等待响应") == 0) {
+        led.stopAnimation()
+        设置当前阶段("放弃番茄钟")
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    if (全局阶段str.compare("工作倒计时") == 0) {
+        设置当前阶段("等待响应")
+        if (番茄时间长度m - 当前倒计时时间ms / 60000 <= 5) {
+            basic.showString("Retry Or GiveUp?")
+        } else {
+            basic.showString("GiveUp?")
+        }
+    } else if (全局阶段str.compare("等待响应") == 0) {
+        led.stopAnimation()
+        恢复前一阶段()
     }
 })
 function 进入倒计时 (阶段str: string, 时长m: number) {
-    当前时段起始时间ms = input.runningTime()
-    当前倒计时时间ms = 时长m * 60000
-    while (全局阶段str.compare(阶段str) == 0) {
-        basic.pause(100)
-        if (局部阶段num == 0) {
-            当前倒计时时间ms = 当前倒计时时间ms - (input.runningTime() - 当前时段起始时间ms)
-            当前时段起始时间ms = input.runningTime()
-            if (当前倒计时时间ms < 0) {
-                设置当前阶段("倒计时结束")
+    if (全局阶段str.compare(阶段str) == 0) {
+        当前时段起始时间ms = input.runningTime()
+        当前倒计时时间ms = 时长m * 60000
+        while (全局阶段str.compare(阶段str) == 0 || 全局阶段str.compare("等待响应") == 0) {
+            basic.pause(100)
+            if (全局阶段str.compare(阶段str) == 0) {
+                if (局部阶段num == 0) {
+                    当前倒计时时间ms = 当前倒计时时间ms - (input.runningTime() - 当前时段起始时间ms)
+                    当前时段起始时间ms = input.runningTime()
+                    if (当前倒计时时间ms < 0) {
+                        设置当前阶段("倒计时结束")
+                    }
+                }
+            } else {
+                当前时段起始时间ms = input.runningTime()
             }
         }
     }
 }
 function 设置当前阶段 (阶段Str: string) {
+    前一全局阶段str = 全局阶段str
+    前一局部阶段num = 局部阶段num
     全局阶段str = 阶段Str
     局部阶段num = 0
+}
+function 恢复前一阶段 () {
+    全局阶段str = 前一全局阶段str
+    局部阶段num = 前一局部阶段num
 }
 input.onButtonPressed(Button.B, function () {
     if (全局阶段str.compare("问候语") == 0) {
@@ -92,8 +125,9 @@ input.onButtonPressed(Button.B, function () {
         if (局部阶段num > 2) {
             设置当前阶段("工作倒计时")
         }
-    } else {
-    	
+    } else if (全局阶段str.compare("等待响应") == 0) {
+        led.stopAnimation()
+        设置当前阶段("所有任务结束")
     }
 })
 function 初始化状态 () {
@@ -110,6 +144,9 @@ function 初始化状态 () {
     // 长时间倒计时
     // 倒计时结束
     // 
+    // 等待响应
+    // 放弃番茄钟
+    // 
     // 所有任务结束
     全局阶段str = "问候语"
     局部阶段num = 0
@@ -120,18 +157,27 @@ function 初始化状态 () {
     当前闪烁状态01 = 0
     番茄钟累积num = 0
 }
+input.onGesture(Gesture.ScreenUp, function () {
+    if (全局阶段str.compare("工作倒计时") == 0) {
+        if (局部阶段num == 1) {
+            局部阶段num = 0
+        }
+    }
+})
 let 番茄钟累积num = 0
-let 当前倒计时时间ms = 0
+let 前一局部阶段num = 0
+let 前一全局阶段str = ""
 let 当前时段起始时间ms = 0
+let 当前倒计时时间ms = 0
 let 长休息长度m = 0
 let 短休息长度m = 0
 let 番茄时间长度m = 0
-let 局部阶段num = 0
 let 当前闪烁状态01 = 0
 let 绘图索引Y = 0
 let 临时值 = 0
 let 绘图索引X = 0
 let 上次绘制的时间m = 0
+let 局部阶段num = 0
 let 全局阶段str = ""
 初始化状态()
 basic.showString("Tomato Clock")
@@ -139,14 +185,19 @@ basic.showString("Tomato Clock")
 basic.forever(function () {
     等待阶段结束("设置")
     while (全局阶段str.compare("所有任务结束") != 0) {
+        if (全局阶段str.compare("放弃番茄钟") != 0) {
+            设置当前阶段("工作倒计时")
+        }
         进入倒计时("工作倒计时", 番茄时间长度m)
-        番茄钟累积num += 1
-        if (番茄钟累积num % 4 == 0) {
-            设置当前阶段("长时间倒计时")
-            进入倒计时("长时间倒计时", 长休息长度m)
-        } else {
-            设置当前阶段("短时间倒计时")
-            进入倒计时("短时间倒计时", 短休息长度m)
+        if (全局阶段str.compare("倒计时结束") != 0) {
+            番茄钟累积num += 1
+            if (番茄钟累积num % 4 == 0) {
+                设置当前阶段("长时间倒计时")
+                进入倒计时("长时间倒计时", 长休息长度m)
+            } else {
+                设置当前阶段("短时间倒计时")
+                进入倒计时("短时间倒计时", 短休息长度m)
+            }
         }
     }
     while (true) {
@@ -169,7 +220,7 @@ control.inBackground(function () {
             if (局部阶段num == 0) {
                 显示符号时间(Math.ceil(当前倒计时时间ms / 60000), true)
             } else if (局部阶段num == 1) {
-            	
+                显示符号时间(Math.ceil(当前倒计时时间ms / 60000), false)
             }
         } else {
         	
