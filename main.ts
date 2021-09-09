3bc3d6930fb10fa2ad406acf7548fc537b91997e
max <maxzhao@users.noreply.github.com>
2021-09-10 00:11:05 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+function 等待阶段结束 (阶段Str: string) {
+    while (阶段Str.compare("设置") == 0) {
+        basic.pause(100)
+    }
+}
 // 番茄时长 25 到 60
 // 
 // 短休息 3 到 5
@@ -61,14 +66,31 @@ input.onButtonPressed(Button.A, function () {
     	
     }
 })
+function 进入倒计时 (阶段str: string, 时长m: number) {
+    当前时段起始时间ms = input.runningTime()
+    当前倒计时时间ms = 时长m * 60000
+    while (全局阶段str.compare(阶段str) == 0) {
+        basic.pause(100)
+        if (局部阶段num == 0) {
+            当前倒计时时间ms = 当前倒计时时间ms - (input.runningTime() - 当前时段起始时间ms)
+            当前时段起始时间ms = input.runningTime()
+            if (当前倒计时时间ms < 0) {
+                设置当前阶段("倒计时结束")
+            }
+        }
+    }
+}
+function 设置当前阶段 (阶段Str: string) {
+    全局阶段str = 阶段Str
+    局部阶段num = 0
+}
 input.onButtonPressed(Button.B, function () {
     if (全局阶段str.compare("问候语") == 0) {
         led.stopAnimation()
     } else if (全局阶段str.compare("设置") == 0) {
         局部阶段num += 1
         if (局部阶段num > 2) {
-            全局阶段str = "工作倒计时"
-            局部阶段num = 0
+            设置当前阶段("工作倒计时")
         }
     } else {
     	
@@ -84,6 +106,11 @@ function 初始化状态 () {
     // 工作倒计时
     // >0: 正常
     // >1: 暂停
+    // 短时间倒计时
+    // 长时间倒计时
+    // 倒计时结束
+    // 
+    // 所有任务结束
     全局阶段str = "问候语"
     局部阶段num = 0
     番茄时间长度m = 25
@@ -91,36 +118,42 @@ function 初始化状态 () {
     长休息长度m = 15
     上次绘制的时间m = -1
     当前闪烁状态01 = 0
+    番茄钟累积num = 0
 }
+let 番茄钟累积num = 0
 let 当前倒计时时间ms = 0
 let 当前时段起始时间ms = 0
 let 长休息长度m = 0
 let 短休息长度m = 0
 let 番茄时间长度m = 0
 let 局部阶段num = 0
+let 全局阶段str = ""
 let 当前闪烁状态01 = 0
 let 绘图索引Y = 0
 let 临时值 = 0
 let 绘图索引X = 0
 let 上次绘制的时间m = 0
-let 全局阶段str = ""
 初始化状态()
 basic.showString("Tomato Clock")
-全局阶段str = "设置"
+设置当前阶段("设置")
 basic.forever(function () {
-    while (全局阶段str.compare("设置") == 0) {
-        basic.pause(100)
-    }
-    当前时段起始时间ms = input.runningTime()
-    当前倒计时时间ms = 番茄时间长度m * 60000
-    while (全局阶段str.compare("工作倒计时") == 0) {
-        basic.pause(100)
-        if (局部阶段num == 0) {
-            当前倒计时时间ms = 当前倒计时时间ms - (input.runningTime() - 当前时段起始时间ms)
-            当前时段起始时间ms = input.runningTime()
+    等待阶段结束("设置")
+    while (全局阶段str.compare("所有任务结束") != 0) {
+        进入倒计时("工作倒计时", 番茄时间长度m)
+        番茄钟累积num += 1
+        if (番茄钟累积num % 4 == 0) {
+            设置当前阶段("长时间倒计时")
+            进入倒计时("长时间倒计时", 长休息长度m)
+        } else {
+            设置当前阶段("短时间倒计时")
+            进入倒计时("短时间倒计时", 短休息长度m)
         }
     }
-    basic.showString("SetTime")
+    while (true) {
+        basic.showString(" U Win:")
+        basic.showString("" + (番茄钟累积num))
+        basic.pause(10000)
+    }
 })
 control.inBackground(function () {
     while (true) {
